c7aa69d192f2ccbca42886ba9450c4b106742142
Rauce <ross@thesaurussoftware.com>
2014-04-11 16:12:16 +0100

    Change double hyphen to single hyphen in 'press any key' prompt

--- GitHubTestA/Program.cs
+++ GitHubTestA/Program.cs
@@ -9,7 +9,7 @@ namespace GitHubTestA {
 			Console.WriteLine("Your GUID is: {0}", Utilities.NewGuid());
 
 			Console.WriteLine();
-			Console.WriteLine("-- Press any key to continue --");
+			Console.WriteLine("- Press any key to continue -");
 			Console.ReadKey();
 
 		}
